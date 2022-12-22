a56ce55fd80dc0f4250924b44513c3c5ffd94a28
parvezkhusro <parvezkhusro@live.com>
2022-12-22 11:39:59 +0530

    Read and update data to database

--- src/main/java/com/parvezkhusro/webserver/WebserverApplication.java
+++ src/main/java/com/parvezkhusro/webserver/WebserverApplication.java
@@ -1,15 +1,10 @@
 package com.parvezkhusro.webserver;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}, scanBasePackages = {"com.parvezkhusro"})
+@SpringBootApplication()
 public class WebserverApplication {
-    private static final Logger log = LoggerFactory.getLogger(WebserverApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(WebserverApplication.class, args);
     }
